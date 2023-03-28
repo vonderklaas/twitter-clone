a20af6678920e6c054ac1505288e10b89cf61a9a
Nick Garbalau <garbalaunick@gmail.com>
2023-03-28 16:06:27 +0300

    Added configurations & basic layout components

---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useRouter } from 'next/router';
+import { BsTwitter } from 'react-icons/bs';
+
+export const SidebarLogo = () => {
+  const router = useRouter();
+  return (
+    <div
+      onClick={() => router.push('/')}
+      className='
+      rounded-full 
+      h-14 
+      w-14 p-4 
+      flex 
+      items-center 
+      justify-center
+    hover:bg-blue-300 
+      hover:bg-opacity-10 
+      cursor-pointer 
+      transition'
+    >
+      <BsTwitter size={26} color='white' />
+    </div>
+  );
+};
